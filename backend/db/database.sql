b2d081c775ea069999c9ed56a8c876768eea054e
Ishaan Gupta <ishaang12@gmail.com>
2021-02-08 17:07:53 +0530

    Fix typo/auto format in db creation query

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -2,7 +2,9 @@
 -- create production database
 CREATE DATABASE xmeme;
 -- connect to the newly created database
-\ c xmeme CREATE TABLE memes(
+\c xmeme 
+
+CREATE TABLE memes(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     url VARCHAR NOT NULL,
@@ -15,7 +17,9 @@ CREATE INDEX created_desc on memes(created desc);
 -- create test database
 CREATE DATABASE xmeme_test;
 -- connect to the newly created database
-\ c xmeme_test CREATE TABLE memes(
+\c xmeme_test 
+
+CREATE TABLE memes(
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     url VARCHAR NOT NULL,
